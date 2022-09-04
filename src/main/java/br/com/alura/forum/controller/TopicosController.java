e35491a58b41beed1aba51fb01f97a43d06c1629
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-09-04 16:31:32 -0300

    Adicionando novo DTO para detalhar Tópicos e adicionando novo método no controller para retornar um tópico em específico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDTO;
 import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -51,4 +53,10 @@ public class TopicosController {
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
 	
+	@GetMapping("/{id}")
+	public DetalhesDoTopicoDTO detalharTopico(@PathVariable Long id) {
+		Topico topico = topicoRepository.getReferenceById(id);
+		
+		return new DetalhesDoTopicoDTO(topico);
+	}
 }
