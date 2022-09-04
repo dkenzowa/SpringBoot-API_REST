759093b20f7212403f4f658d567257142bad2ddc
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-09-04 06:42:49 -0300

    Adicionando query feita pelo JPA e filtro do nome do curso

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,11 +1,15 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	List<Topico> findByCursoNome(String nomeCurso);
 	
-	
-	
+	//Se houver atributo com o mesmo nome de um relacionamento no banco, usamos _ para remover a ambiguidade e forçar o JPA a escolher o relacionamento com Curso
+	List<Topico> findByCurso_Nome(String nomeCurso);
 }
