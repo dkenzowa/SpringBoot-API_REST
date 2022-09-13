526d96f951f4685cbe5e7f1e2558d720fb90bd47
Danilo Kenzo <dani.kenzo@hotmail.com>
2022-09-13 16:49:28 -0300

    Adicionando Spring Security e autenticação

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -3,8 +3,11 @@ package br.com.alura.forum.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,18 +15,36 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
 
 	@Bean
+    UserDetailsService userDetailsService() {
+        return new AutenticacaoService();
+    }
+ 
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+         
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+     
+        return authProvider;
+    }
+	
+	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((authz) -> {
 			try {
-				authz
-				.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-				.anyRequest().authenticated()
-				.and().formLogin();
+				authz.antMatchers(HttpMethod.GET, "/topicos").permitAll().antMatchers(HttpMethod.GET, "/topicos/*")
+						.permitAll().anyRequest().authenticated().and().formLogin();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}).httpBasic();
+
 		return http.build();
 	}
 
